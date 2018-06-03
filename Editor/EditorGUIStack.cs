61384be71a7cf26131d7c5efa0008268bb05a2dd
Ukidabek <ukidabek@gmail.com>
2018-06-03 09:05:36 +0200

    - Game manger added

---
+++ Editor/EditorGUIStack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEditor
+{
+    public class EditorGUIStack<T>
+    {
+        private Stack<T> stack = new Stack<T>();
+
+        public void SetValue(ref T valueToSet, T newValue)
+        {
+            stack.Push(valueToSet);
+            valueToSet = newValue;
+        }
+
+        public void RevertValue(ref T valueTorevert)
+        {
+            valueTorevert = stack.Pop();
+        }
+    }
+}
---
+++ Editor/EditorGUIStackTest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace UnityEditor
+{
+    public class EditorGUIStackTest
+    {
+        [Test]
+        public void EditorGUIStack_Base_Test()
+        {
+            EditorGUIStack<float> floatStack = new EditorGUIStack<float>();
+
+            float value = 5f;
+
+            floatStack.SetValue(ref value, 10f);
+
+            Assert.AreEqual(10f, value);
+
+            floatStack.RevertValue(ref value);
+
+            Assert.AreEqual(5f, value);
+        }
+
+        [Test]
+        public void EditorGUIStack_Stacking_Test_Test()
+        {
+            EditorGUIStack<Color> colorStack = new EditorGUIStack<Color>();
+
+            Color color1 = Color.black;
+            Color color2 = Color.blue;
+
+            colorStack.SetValue(ref color1, Color.yellow);
+            Assert.AreEqual(Color.yellow, color1);
+            colorStack.SetValue(ref color2, Color.yellow);
+            Assert.AreEqual(Color.yellow, color2);
+            colorStack.RevertValue(ref color2);
+            Assert.AreEqual(Color.blue, color2);
+            colorStack.RevertValue(ref color1);
+            Assert.AreEqual(Color.black, color1);
+        }
+    }
+}
